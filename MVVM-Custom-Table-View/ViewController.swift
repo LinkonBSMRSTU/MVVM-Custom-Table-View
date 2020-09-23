2839f4a7987cfa0474cb8ed60ce270219004e51f
Fazle Rabbi Linkon <fazle.linkon@dreamonline.co.jp>
2020-09-23 15:29:38 +0600

    Initial Commit

---
+++ MVVM-Custom-Table-View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Custom-Table-View
+//
+//  Created by Fazle Rabbi Linkon on 23/9/20.
+//  Copyright © 2020 Fazle Rabbi Linkon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
